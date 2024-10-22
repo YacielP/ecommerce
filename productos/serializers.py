917721ca1ed4c04ec645344bcf1ad4f47c369292
Yaciel Perez Alvarez <yacielpa@estudiantes.uci.cu>
2024-10-22 00:21:59 -0400

    Serializadores creados

---
+++ productos/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Tienda, Producto, Inventario
+
+class TiendaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tienda
+        fields = '__all__'
+
+    def validate_propietario(self, value):
+        if value.rol != 'tienda':
+            raise serializers.ValidationError('Solo los usuarios con el rol de "tienda" pueden ser propietarios de una tienda.')
+        return value
+    
+class Producto(serializers.ModelSerializer):
+    class Meta:
+        model = Producto
+        fields = '__all__'
+
+class Inventario(serializers.ModelSerializer):
+    class Meta:
+        model = Inventario
+        fields = ['id', 'tienda', 'producto', 'cantidad']
+
+    def validate(self, data):
+        if data['cantidad'] < 0:
+            raise serializers.ValidationError("La cantidad no puede ser negativa.")
+        return data
