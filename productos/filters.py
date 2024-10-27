4424305187ea926a4e8114bbd0d5cd3b892bff55
Yaciel Perez Alvarez <yacielpa@estudiantes.uci.cu>
2024-10-27 12:04:15 -0400

    Agregando filtros al catalogo

---
+++ productos/filters.py
@@ -0,0 +1,11 @@
+import django_filters
+from .models import InventarioProducto
+
+class InventarioProductoFilter(django_filters.FilterSet):
+    nombre_producto = django_filters.CharFilter(field_name='producto_central__nombre', lookup_expr='icontains')
+    precio_min = django_filters.NumberFilter(field_name='precio_personalizado', lookup_expr='gte')
+    precio_max = django_filters.NumberFilter(field_name='precio_personalizado', lookup_expr='lte')
+
+    class Meta:
+        model = InventarioProducto
+        fields = ['nombre_producto', 'precio_min', 'precio_max']
