09350ff38d32f500ea731dc8d7ab8246674472ad
Yaciel Perez Alvarez <yacielpa@estudiantes.uci.cu>
2024-11-02 12:16:10 -0400

    Los usuarios ya pueden eliminarse si son dueños de sus cuentas

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions
 from .models import Usuario
 from .serializers import RegistroSerializer
-from .permissions import IsDuenno
+from .permissions import IsOwnerOrAdmin
 
 class UsuarioListView(generics.ListAPIView):
     queryset = Usuario.objects.all()
@@ -17,12 +17,11 @@ class UsuarioDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Usuario.objects.all()
     serializer_class = RegistroSerializer
 
-    """!!!!!!!!!!HAY PROBLEMAS PARA Q UN USUARIO ELIMINE Y ACTUALICE SU PROPIA CUENTA(LO MISMO CON EL ADMIN)"""
     def get_permissions(self):
         if self.request.method == 'GET':
             #Todos  los usuarios pueden ver los detalles del resto de usuarios
-            return [permissions.IsAuthenticated()]
+            return [permissions.IsAdminUser()]
         elif self.request.method in ['PUT', 'DELETE']:
             #Solo los usuarios dueños de sus cuentas o administradores pueden eliminarla o actualizarla
-            return [permissions.IsAdminUser(), IsDuenno()]
+            return [IsOwnerOrAdmin()]
         return [permissions.IsAuthenticated()]  # Default a `IsAuthenticated` para otros métodos
