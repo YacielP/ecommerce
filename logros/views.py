207d8ba6a905c4e2c5bc2ec3fc8f2684db602e9b
Yaciel <yacielp20@gmial.com>
2024-11-12 09:34:03 +0100

    Permiso para LogroViewSet

--- logros/views.py
+++ logros/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Logro
 from .serializers import LogroSerializer, LogroTiendaSerializer, LogroUsuarioSerializer
 
 class LogroViewSet(viewsets.ModelViewSet):
     queryset = Logro.objects.all()
     serializer_class = LogroSerializer
+    permission_classes = [permissions.IsAdminUser]
