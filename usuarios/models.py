6734423ff7342aa92f5d8fc457df3fc707c4ce1f
Yaciel <yacielp20@gmial.com>
2024-11-15 10:42:29 +0100

    Agregando unicidad a los campos email y username de Usuario

--- usuarios/models.py
+++ usuarios/models.py
@@ -11,6 +11,9 @@ class Usuario(AbstractUser):
     direccion = models.CharField(max_length=500, blank=True, null=True)
     telefono = models.CharField(max_length=20, blank=True, null=True)
 
+    class Meta:
+        unique_together = ('username', 'email')
+
     def __str__(self):
         return self.username
 
