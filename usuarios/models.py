3f05b5e5bcfc9c11e5e30864db046e5799664198
Yaciel <yacielp20@gmial.com>
2024-11-15 10:12:28 +0100

    Creando herencia: UsuarioComprador ahora tiene puntos

--- usuarios/models.py
+++ usuarios/models.py
@@ -13,3 +13,18 @@ class Usuario(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class UsuarioComprador(Usuario):
+    puntos = models.IntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        if self.rol != 'comprador':
+            raise ValueError("El rol debe de ser 'comprador' para un Comprador")
+        super().save(*args, **kwargs)
+
+class UsuarioPropietario(Usuario):
+    
+    def save(self, *args, **kwargs):
+        if self.rol != 'tienda':
+            raise ValueError("El rol debe de ser 'tienda' para un Propietario")
+        super().save(*args, **kwargs)
