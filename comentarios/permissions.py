8f3979816fd67194395e386f1a68444e0a9bb9a9
Yaciel <yacielp20@gmial.com>
2024-11-16 11:02:39 +0100

    Sustituyendo el nombre del rol 'tienda' en permisos de comentarios por 'propietario'

--- comentarios/permissions.py
+++ comentarios/permissions.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import BasePermission
 
 class IsOwnerComentario(BasePermission):
     def has_permission(self, request, view):
-        return request.user.rol in ['comprador', 'tienda']
+        return request.user.rol in ['comprador', 'propietario']
     
     def has_object_permission(self, request, view, obj):
         return obj.usuario == request.user
