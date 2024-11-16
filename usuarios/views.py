6715422bf483ecc02f5de96a70d2a4815215740d
Yaciel <yacielp20@gmial.com>
2024-11-16 10:25:40 +0100

    Vistas Usuarios_app: Agregando viewsets para Comprador

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,27 +1,36 @@
 from rest_framework import generics, permissions
-from .models import Usuario
-from .serializers import RegistroSerializer
-from .permissions import IsOwnerOrAdmin
+from .models import Usuario, UsuarioComprador, UsuarioPropietario
+from .serializers import RegistroSerializer, CompradorSerializer, PropietarioSerializer, UpdateAdminSerializer, UpdateCompradorSerializer
+from .permissions import IsAdminOrPropieratio, IsOwnerOrAdminOrPropietario
+from rest_framework import viewsets
 
 class UsuarioListView(generics.ListAPIView):
     queryset = Usuario.objects.all()
     serializer_class = RegistroSerializer
     permission_classes = [permissions.IsAdminUser]
-
-class UsuarioCreateView(generics.CreateAPIView):
-    queryset = Usuario.objects.all()
-    serializer_class = RegistroSerializer
-    permission_classes = [permissions.AllowAny]
-
-class UsuarioDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Usuario.objects.all()
-    serializer_class = RegistroSerializer
+    
+class UsuarioCompradorViewSet(viewsets.ModelViewSet):
+    queryset = UsuarioComprador
+    serializer_class = CompradorSerializer
 
     def get_permissions(self):
-        if self.request.method == 'GET':
-            #Todos  los usuarios pueden ver los detalles del resto de usuarios
-            return [permissions.IsAdminUser()]
-        elif self.request.method in ['PUT', 'DELETE']:
-            #Solo los usuarios dueños de sus cuentas o administradores pueden eliminarla o actualizarla
-            return [IsOwnerOrAdmin()]
-        return [permissions.IsAuthenticated()]  # Default a `IsAuthenticated` para otros métodos
+        if self.action == 'list':
+            return [IsAdminOrPropieratio()]
+        elif self.action == 'retrieve':
+            return [IsOwnerOrAdminOrPropietario()]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            return [IsOwnerOrAdminOrPropietario()]
+        return super().get_permissions()
+    
+    def get_serializer_class(self):
+        if self.action in ['update', 'partial_update']:
+            #Verificar si el usuario es administrador
+            if self.request.user.is_staff:
+                return UpdateAdminSerializer
+            return UpdateCompradorSerializer
+        return UpdateCompradorSerializer
+    
+    def get_object(self):
+        obj = super().get_object()
+        self.check_object_permissions(self.request, obj)
+        return obj
