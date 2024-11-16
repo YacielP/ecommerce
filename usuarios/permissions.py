b8bcc25dd74180b6a5453b81eb87a9e925fdfabd
Yaciel <yacielp20@gmial.com>
2024-11-16 10:48:45 +0100

    Permisos Usuarios_app: permisos para la clase propietarioViewSet

--- usuarios/permissions.py
+++ usuarios/permissions.py
@@ -14,6 +14,9 @@ class IsOwnerOrAdmin(BasePermission):
         if request.user.is_staff or obj == request.user:
             return True
         
+"""
+    Para CompradoresViewSet:
+"""
 class IsAdminOrPropieratio(BasePermission):
     def has_permission(self, request, view):
         #Verificar si el usuario es administrador o propietario
@@ -21,9 +24,25 @@ class IsAdminOrPropieratio(BasePermission):
     
 class IsOwnerOrAdminOrPropietario(BasePermission):
     def has_object_permission(self, request, view, obj):
-        #Permitir solo metodos seguros
+        #Permitir solo metodos seguros a administradores, propietarios y propios usuarios
         if request.method is SAFE_METHODS:
             return request.user.is_staff or obj == request.user or request.user.rol == 'propietario'
         #Permitir solo a los admins y a los propios usuarios eliminar o editar
         return request.user.is_staff or obj == request.user
         
+
+"""
+    Para PropietariosViewSet:
+"""
+class IsAdminOrComprador(BasePermission):
+    def has_permission(self, request, view):
+        #Verificar si el usuario es administrador o comprador
+        return request.user.is_staff or request.user.rol == 'comprador'
+    
+class IsOwnerOrAdminOrComprador(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        #Permitir solo metodos seguros a administradores, compradores y propios usuarios
+        if request.method is SAFE_METHODS:
+            return request.user.is_staff or obj == request.user or request.user.rol == 'comprador'
+        #Permitir solo a los admins y a los propios usuarios eliminar o editar
+        return request.user.is_staff or obj == request.user
