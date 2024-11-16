4dcdc430b65775e6e7769c07f9ec74b4a8767ab6
Yaciel <yacielp20@gmial.com>
2024-11-16 11:20:57 +0100

    Cambiando la importacion del modelo Usuario por Comprador

--- usuarios/signals.py
+++ usuarios/signals.py
@@ -1,9 +1,9 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Usuario
+from .models import UsuarioComprador
 from pedidos.models import Carrito
 
-@receiver(post_save, sender=Usuario)
+@receiver(post_save, sender=UsuarioComprador)
 def crear_carrito_para_comprador(sender, instance, created, **kwargs):
     if created and instance.rol == 'comprador':
         Carrito.objects.create(usuario=instance)
