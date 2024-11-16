b28afed2dc50979d5958663ff7411b457e874251
Yaciel <yacielp20@gmial.com>
2024-11-16 11:21:16 +0100

    Colocando las urls para las viewsets

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,15 +1,19 @@
-from django.urls import path
-from .views import UsuarioListView, UsuarioCreateView, UsuarioDetailView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UsuarioListView, UsuarioCompradorViewSet, UsuarioPropietarioViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
 
+router = DefaultRouter()
+router.register(r'propietarios', UsuarioPropietarioViewSet, basename='propietario')
+router.register(r'compradores', UsuarioCompradorViewSet, basename='comprador')
+
 urlpatterns = [
     path('', UsuarioListView.as_view(), name='usuario-list'),
-    path('registrar/', UsuarioCreateView.as_view(), name='usuario-create'),
-    path('usuario/<int:pk>/', UsuarioDetailView.as_view(), name='usuario-detail'),
+    path('rol/', include(router.urls)),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
