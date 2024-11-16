ed5836167623cc3883fde23f3e27294c9cd7100a
Yaciel <yacielp20@gmial.com>
2024-11-16 10:49:22 +0100

    Agregando la clase PropietarioViewset

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions
 from .models import Usuario, UsuarioComprador, UsuarioPropietario
 from .serializers import RegistroSerializer, CompradorSerializer, PropietarioSerializer, UpdateAdminSerializer, UpdateCompradorSerializer
-from .permissions import IsAdminOrPropieratio, IsOwnerOrAdminOrPropietario
+from .permissions import IsAdminOrPropieratio, IsOwnerOrAdminOrPropietario, IsAdminOrComprador, IsOwnerOrAdminOrComprador
 from rest_framework import viewsets
 
 class UsuarioListView(generics.ListAPIView):
@@ -30,6 +30,28 @@ class UsuarioCompradorViewSet(viewsets.ModelViewSet):
             return UpdateCompradorSerializer
         return UpdateCompradorSerializer
     
+    def get_object(self):
+        obj = super().get_object()
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+class UsuarioPropietarioViewSet(viewsets.ModelViewSet):
+    queryset = UsuarioPropietario.objects.all()
+    serializer_class = PropietarioSerializer
+
+    def get_permissions(self):
+        if self.action == 'list':
+            return [IsAdminOrComprador()]
+        elif self.action == 'retrieve':
+            return [IsOwnerOrAdminOrComprador()]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            return [IsOwnerOrAdminOrComprador()]
+        return super().get_permissions()
+    
+    def get_serializer_class(self):
+        if self.action in ['update', 'partial_update']:
+            return UpdateAdminSerializer
+        
     def get_object(self):
         obj = super().get_object()
         self.check_object_permissions(self.request, obj)
